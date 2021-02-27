cdb7611f8ab85ad1629e4a427b751ecbb09c7dc6
ecem <ecemsenaunlu@gmail.com>
2021-02-27 22:53:40 +0300

    .

--- Core/Utilities/Security/Hashing/HashingHelper.cs
+++ Core/Utilities/Security/Hashing/HashingHelper.cs
@@ -19,8 +19,16 @@ namespace Core.Utilities.Security.Hashing
             using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
             {
                 var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+                for (int i = 0; i < computedHash.Length; i++)
+                {
+                    if (computedHash[i]!=passwordHash[i])
+                    {
+                        return false;
+                    }
+                }
+                return true;
             }
-
+            
         }
     }
 }
